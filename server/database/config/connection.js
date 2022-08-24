9f09624ff45164395c5f7b6f942659ce0653cdf2
abdallah-alshawaf <abdallah.i.alshawaf@gmail.com>
2022-08-24 15:51:49 +0300

    heroku

--- server/database/config/connection.js
+++ server/database/config/connection.js
@@ -3,7 +3,10 @@ require('dotenv').config();
 
 const connection = new Pool({
     connectionString: process.env.DB_URL,
-    ssl: false   
+    ssl: {
+        rejectUnauthorized: false
+      }
+       
 } 
 )
 
